096dbdbe9054cb4f83bd5be3eb9ad64ff745bc17
drswitchoff <wedgelever@gmail.com>
2014-09-27 11:26:40 -0700

    Adds text control box to GUI
    Text control was meant to be "stretchable."
    Console still opens on execution.

--- include/Main.h
+++ include/Main.h
@@ -4,12 +4,15 @@
 #include <wx/choicebk.h>
 #include <wx/notebook.h>
 #include <wx/panel.h>
+#include <wx/textctrl.h>
 
 #ifndef WX_PRECOMP
     #include <wx/wx.h>
 #endif
 
 #define gbAppSize wxSize(550,250)
+
+
 
 class BaseFrame: public wxFrame
 {
@@ -20,7 +23,10 @@ private:
     void OnExit(wxCommandEvent& event);
     void OnAbout(wxCommandEvent& event);
     wxChoicebook *DropDown;                     // Create an obj for Dropdown list of type wxChoicebook
-
+	wxTextCtrl* CourseDisplay;                  // Declare a text control box calles course display
+	
+	
+    
     wxDECLARE_EVENT_TABLE();
 };
 
@@ -28,5 +34,6 @@ enum
 {
     ID_AddCourseMenuSelect = 1,
     ID_ChoiceBook = 2,
-    ID_GradeBookPanel = 3
+    ID_GradeBookPanel = 3,
+	ID_CourseDisplay = 4
 };
--- src/Main.cc
+++ src/Main.cc
@@ -11,6 +11,7 @@ wxEND_EVENT_TABLE()
 BaseFrame::BaseFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
         : wxFrame(NULL, wxID_ANY, title, pos, size)
 {
+	// create standard generic pbjects
     wxMenu *menuFile = new wxMenu;
     menuFile->Append(ID_AddCourseMenuSelect, "&Add Course...\tCtrl-G",
                      "Add a course to your GradeBook");
@@ -29,8 +30,18 @@ BaseFrame::BaseFrame(const wxString& title, const wxPoint& pos, const wxSize& si
     wxBoxSizer *BaseBoxSizer = new wxBoxSizer(wxVERTICAL);
     wxPanel *BaseFramePanel = new wxPanel(this, ID_GradeBookPanel, wxPoint(0,0), gbAppSize, wxTAB_TRAVERSAL, "BaseFramePanel");
     BaseBoxSizer->Add(BaseFramePanel,0,0,0);
+	
+	// Puts dropdown and text control boxes in front sizer
+	wxBoxSizer *FrontBoxSizer = new wxBoxSizer(wxVERTICAL);
 	DropDown = new wxChoicebook(BaseFramePanel, ID_ChoiceBook, wxPoint(30,30), wxSize(150,20), 0, "DropDownChoice");
-	BaseBoxSizer->Fit(this);
+	FrontBoxSizer->Add(ID_ChoiceBook, 1, wxEXPAND | wxALL, 10);
+	CourseDisplay = new wxTextCtrl(BaseFramePanel, ID_CourseDisplay, _("<Enter text here to open dialog box>"), wxPoint(100,100), wxSize(300,100), wxTE_MULTILINE, wxDefaultValidator, _T("ID_CourseDisplay"));
+	FrontBoxSizer->Add(ID_CourseDisplay, 1, wxEXPAND | wxALL, 10);
+	
+	// Causes parents objects to fit
+    BaseBoxSizer->Fit(this);
+	FrontBoxSizer->Fit(BaseFramePanel);
+	
 
 }
 void BaseFrame::OnExit(wxCommandEvent& event)
